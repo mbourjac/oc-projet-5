c423290c97a59f95384da1f73b812da2bdbe6287
mbourjac2 <michael.bourjac@gmail.com>
2023-01-15 15:17:55 +0100

    Rework createCartProduct() to avoid using innerHTML

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,5 @@
 import { getData } from "./modules/get-data.js";
+import { createProductImage } from "./modules/create-product-image.js";
 import { getStorageData } from "./modules/get-storage-data.js";
 import { setStorageData } from "./modules/set-storage-data.js";
 import { setApiPath } from "./modules/set-api-path.js";
@@ -8,7 +9,8 @@ createDynamicCart();
 async function createDynamicCart() {
     const storedProducts = getStorageData();
 
-    await Promise.all(storedProducts.map(createCartItem));
+    const cartProduct = await Promise.all(storedProducts.map(createCartProduct));
+    appendCartProduct(cartProduct);
 
     handleRemoveButtons();
     handleQuantityInputs();
@@ -16,36 +18,132 @@ async function createDynamicCart() {
     handleOrderForm();
 }
 
-async function createCartItem(storedProduct) {
+async function createCartProduct(storedProduct) {
+    const {id, color, quantity} = storedProduct;
+    const productData = await getData(id);
+
+    const productArticle = document.createElement("article");
+    const productImageContainer = createProductImageContainer(productData);
+    const productContent = createProductContent(productData, color, quantity);
+
+    productArticle.classList.add("cart__item");
+    productArticle.dataset.id = id;
+    productArticle.dataset.color = color;
+    productArticle.dataset.price = productData.price;
+    productArticle.append(productImageContainer, productContent);
+
+    return productArticle;
+}
+
+function createProductImageContainer(productData) {
+    const productImageContainer = document.createElement("div");
+    const productImage = createProductImage(productData);
+
+    productImageContainer.classList.add("cart__item__img");
+    productImageContainer.append(productImage);
+
+    return productImageContainer;
+}
+
+function createProductContent(productData, color, quantity) {
+    const productContent = document.createElement("div");
+    const productDescription = createProductDescription(productData, color);
+    const productSettings = createProductSettings(quantity);
+
+    productContent.classList.add("cart__item__content");
+    productContent.append(productDescription, productSettings);
+
+    return productContent;
+}
+
+function createProductDescription(productData, color) {
+    const productDescription = document.createElement("div");
+    const productName = createProductName(productData);
+    const productColor = createProductColor(color);
+    const productPrice = createProductPrice(productData);
+
+    productDescription.classList.add("cart__item__content__description");
+    productDescription.append(productName, productColor, productPrice);
+
+    return productDescription;
+}
+
+function createProductName({ name }) {
+    const productName = document.createElement("h2");
+
+    productName.textContent = name;
+
+    return productName;
+}
+
+function createProductColor(color) {
+    const productColor = document.createElement("p");
+
+    productColor.textContent = color;
+
+    return productColor;
+}
+
+function createProductPrice({ price }) {
+    const productPrice = document.createElement("p");
+
+    productPrice.textContent = `${price} €`;
+
+    return productPrice;
+}
+
+function createProductSettings(quantity) {
+    const productSettings = document.createElement("div");
+    const productQuantityContainer = createProductQuantityContainer(quantity);
+    const productDeleteContainer = createProductDeleteContainer();
+
+    productSettings.classList.add("cart__item__content__settings");
+    productSettings.append(productQuantityContainer, productDeleteContainer);
+
+    return productSettings;
+}
+
+function createProductQuantityContainer(quantity) {
+    const productQuantityContainer = document.createElement("div");
+    const productQuantityText = document.createElement("p");
+    const productQuantityInput = createQuantityInput(quantity);
+
+    productQuantityContainer.classList.add("cart__item__content__settings__quantity");
+    productQuantityText.textContent = "Qté : ";
+    productQuantityContainer.append(productQuantityText, productQuantityInput);
+
+    return productQuantityContainer;
+}
+
+function createQuantityInput(quantity) {
+    const productQuantityInput = document.createElement("input");
+
+    productQuantityInput.classList.add("itemQuantity");
+    productQuantityInput.type = "number";
+    productQuantityInput.name = "itemQuantity";
+    productQuantityInput.min = "1";
+    productQuantityInput.max = "100";
+    productQuantityInput.value = quantity;
+
+    return productQuantityInput;
+}
+
+function createProductDeleteContainer() {
+    const productDeleteContainer = document.createElement("div");
+    const productDeleteText = document.createElement("p");
+
+    productDeleteContainer.classList.add("cart__item__content__settings__delete");
+    productDeleteText.classList.add("deleteItem");
+    productDeleteText.textContent = "Supprimer";
+    productDeleteContainer.append(productDeleteText);
+
+    return productDeleteContainer;
+}
+
+function appendCartProduct(cartProduct) {
     const productsContainer = document.querySelector("#cart__items");
-    const productId = storedProduct.id;
-    const productColor = storedProduct.color;
-    const productQuantity = storedProduct.quantity;
 
-    const productData = await getData(productId);
-
-    productsContainer.innerHTML +=
-        `<article class="cart__item" data-id="${productId}" data-color="${productColor}" data-price="${productData.price}">
-            <div class="cart__item__img">
-                <img src="${productData.imageUrl}" alt="${productData.altTxt}">
-            </div>
-            <div class="cart__item__content">
-                <div class="cart__item__content__description">
-                    <h2>${productData.name}</h2>
-                    <p>${productColor}</p>
-                    <p>${productData.price} €</p>
-                </div>
-                <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                        <p>Qté : </p>
-                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${productQuantity}">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                        <p class="deleteItem">Supprimer</p>
-                    </div>
-                </div>
-            </div>
-        </article>`;
+    productsContainer.append(...cartProduct);
 }
 
 function handleRemoveButtons() {
