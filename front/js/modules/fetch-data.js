9844fe5e6e1d05c351420f38f17042731582f850
mbourjac2 <michael.bourjac@gmail.com>
2023-01-20 18:53:34 +0100

    Refactor error handling

--- front/js/modules/fetch-data.js
+++ front/js/modules/fetch-data.js
@@ -4,10 +4,10 @@ import { redirectToNewPage } from "./redirect-to-new-page.js";
 export async function fetchData(path, options) {
     try {
         const apiEndpoint = setApiEndpoint(path);
-        const response = await fetch(apiEndpoint, options); /* check if options ? */
+        const response = await fetch(apiEndpoint, options);
 
         if (!response.ok) {
-            throw handleError(response);
+            handleError(response);
         }
 
         const data = await response.json();
@@ -16,17 +16,17 @@ export async function fetchData(path, options) {
     } catch (error) {
         alert("Une erreur est survenue.");
         console.error(error);
-        /* redirectToNewPage("index.html"); */
+        redirectToNewPage("index.html");
     }
 }
 
 function handleError(response) {
     switch(response.status) {
         case 404:
-            return new Error("Not Found: The requested resource could not be found.");
+            throw new Error("Not Found: The requested resource could not be found.");
         case 500:
-            return new Error("Internal Server Error: There was a problem with the server.");
+            throw new Error("Internal Server Error: There was a problem with the server.");
         default:
-            return new Error(`HTTP error! status: ${response.status}`);
+            throw new Error(`HTTP error! status: ${response.status}`);
     }
 }
