80416382591d4a683aebc087320321197304495d
mbourjac2 <michael.bourjac@gmail.com>
2023-01-22 11:42:34 +0100

    Rework fetch error handling with classes

--- front/js/modules/fetch-data.js
+++ front/js/modules/fetch-data.js
@@ -7,26 +7,52 @@ export async function fetchData(path, options) {
         const response = await fetch(apiEndpoint, options);
 
         if (!response.ok) {
-            handleError(response);
+            handleHttpError(response);
         }
 
         const data = await response.json();
 
         return data;
     } catch (error) {
-        alert("Une erreur est survenue.");
-        console.error(error);
+        if (error instanceof HttpError) {
+            alert(error.message);
+            console.error(`${error.name}: status ${error.status}`);
+        } else {
+            alert("Désolé, une erreur est survenue.")
+            console.error(error);
+        }
+
         redirectToNewPage("index.html");
     }
 }
 
-function handleError(response) {
-    switch(response.status) {
+function handleHttpError(response) {
+    switch (response.status) {
         case 404:
-            throw new Error("Not Found: The requested resource could not be found.");
+            throw new NotFoundError();
         case 500:
-            throw new Error("Internal Server Error: There was a problem with the server.");
+            throw new InternalServorError();
         default:
-            throw new Error(`HTTP error! status: ${response.status}`);
+            throw new HttpError(response.statusText, response.status);
+    }
+}
+
+class HttpError extends Error {
+    constructor(message, status) {
+        super(message);
+        this.name = this.constructor.name;
+        this.status = status;
+    }
+}
+
+class NotFoundError extends HttpError {
+    constructor() {
+        super("Désolé, ce produit est introuvable.", 404);
+    }
+}
+
+class InternalServorError extends HttpError {
+    constructor() {
+        super("Désolé, nous rencontrons un problème avec le serveur.", 500);
     }
 }
