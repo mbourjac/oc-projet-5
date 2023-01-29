e115b8c877a508e0ca4e9837ffb880a581550486
mbourjac2 <michael.bourjac@gmail.com>
2023-01-29 15:54:36 +0100

    Rename function parameter

--- front/js/modules/redirect-to-new-page.js
+++ front/js/modules/redirect-to-new-page.js
@@ -1,10 +1,10 @@
 /**
  * Redirects to a new page.
  * @param {string} page - The name of the new page.
- * @param {Object} [parametersObject] - An object containing key-value pairs of parameters to be added to the URL.
+ * @param {Object} [parameters] - An object containing key-value pairs of parameters to be added to the URL.
  */
-export function redirectToNewPage(page, parametersObject) {
-    const newPageUrl = setNewPageUrl(page, parametersObject);
+export function redirectToNewPage(page, parameters) {
+    const newPageUrl = setNewPageUrl(page, parameters);
 
     window.location.assign(newPageUrl);
 }
@@ -12,15 +12,15 @@ export function redirectToNewPage(page, parametersObject) {
 /**
  * Sets the URL of the new page.
  * @param {string} page - The name of the new page.
- * @param {Object} [parametersObject] - An object containing key-value pairs of parameters to be added to the URL.
+ * @param {Object} [parameters] - An object containing key-value pairs of parameters to be added to the URL.
  * @returns {URL} The URL of the new page.
  */
-function setNewPageUrl(page, parametersObject) {
+function setNewPageUrl(page, parameters) {
     const baseUrl = new URL("/front/html/", window.location.href);
     const newPageUrl = new URL(page, baseUrl);
 
-    if (parametersObject) {
-        for (const [key, value] of Object.entries(parametersObject)) {
+    if (parameters) {
+        for (const [key, value] of Object.entries(parameters)) {
             newPageUrl.searchParams.set(key, value);
         }
     }
