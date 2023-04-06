92182663346b809d9231f6f1525ab18315e70f68
mbourjac2 <michael.bourjac@gmail.com>
2023-04-06 19:52:41 +0200

    Add types to setStorageData

--- front/src/scripts/modules/set-storage-data.ts
+++ front/src/scripts/modules/set-storage-data.ts
@@ -1,3 +1,5 @@
-export function setStorageData(storedProducts) {
+import { StorageProduct } from '../interfaces/storage-product';
+
+export function setStorageData(storedProducts: StorageProduct[]): void {
   localStorage.setItem('storedProducts', JSON.stringify(storedProducts));
 }
