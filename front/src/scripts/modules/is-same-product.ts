f940f862edeb9b2f2ffadf8b6b1c582960774e73
mbourjac2 <michael.bourjac@gmail.com>
2023-04-06 19:51:46 +0200

    Add types to isSameProduct

--- front/src/scripts/modules/is-same-product.ts
+++ front/src/scripts/modules/is-same-product.ts
@@ -1,4 +1,11 @@
-export function isSameProduct(firstProduct, secondProduct) {
+import { StorageProduct } from '../interfaces/storage-product';
+
+export type ProductIdentifier = Pick<StorageProduct, 'id' | 'color'>;
+
+export function isSameProduct(
+  firstProduct: ProductIdentifier,
+  secondProduct: ProductIdentifier
+): boolean {
   return (
     firstProduct.id === secondProduct.id &&
     firstProduct.color === secondProduct.color
