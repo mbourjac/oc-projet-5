78c2cfbd3d0f858c3ff5d0518b65095d6cf4a209
mbourjac2 <michael.bourjac@gmail.com>
2023-04-06 19:48:54 +0200

    Add types to navigateToPage

--- front/src/scripts/modules/navigate-to-page.ts
+++ front/src/scripts/modules/navigate-to-page.ts
@@ -1,6 +1,9 @@
-import { setPageUrl } from './set-page-url.js';
+import { setPageUrl } from './set-page-url';
 
-export function navigateToPage(page, parameters) {
+export function navigateToPage(
+  page: string,
+  parameters?: { [key: string]: string }
+): void {
   const newPageUrl = setPageUrl(page, parameters);
 
   window.location.assign(newPageUrl);
