66c97fdf2ee8c931c8a012f2a6cbaf29b945be25
mbourjac2 <michael.bourjac@gmail.com>
2023-04-06 19:45:12 +0200

    Add types to createProductElement

--- front/src/scripts/modules/create-product-element.ts
+++ front/src/scripts/modules/create-product-element.ts
@@ -1,10 +1,11 @@
-export function createProductElement(element, content, className) {
+export function createProductElement<T extends keyof HTMLElementTagNameMap>(
+  element: T,
+  content: string,
+  className?: string
+): HTMLElementTagNameMap[T] {
   const productElement = document.createElement(element);
 
-  if (className) {
-    productElement.classList.add(className);
-  }
-
+  if (className) productElement.classList.add(className);
   productElement.textContent = content;
 
   return productElement;
